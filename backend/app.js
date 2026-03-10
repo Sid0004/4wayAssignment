0dbb680d25d638544b419548bb5530e8b721d1ab
Sid0004 <siddhantsharma4uonly@gmail.com>
2026-03-10 12:53:14 +0530

    database setup and scripts

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+// routes will be added after DB is confirmed
+
+export { app };
